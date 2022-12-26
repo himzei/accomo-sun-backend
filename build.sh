fc4fc9bf7e92f5ccd45915909ca4e5c1380a1178
조현일 <himzei@gmail.com>
2022-12-27 08:46:28 +0900

    c

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
-pip install --upgrade pip
-pip install --force-reinstall -U setuptools
-
 set -o errexit 
 
 poetry install 
 
 python manage.py collectstatic --no-input 
 python manage.py migrate
+
+pip install --upgrade pip
+pip install --force-reinstall -U setuptools
