9ecd7aa46b4c6a08dacc5fae533870fafa6d5a53
조현일 <himzei@gmail.com>
2022-12-27 00:55:34 +0900

    bbbb

--- build.sh
+++ build.sh
@@ -5,4 +5,5 @@ poetry install
 python manage.py collectstatic --no-input 
 python manage.py migrate
 
-pip install --upgrade pippip install --force-reinstall -U setuptools
+pip install --upgrade pip
+pip install --force-reinstall -U setuptools
